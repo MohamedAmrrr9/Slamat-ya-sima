476c7b41a9cd4077d97c31e211c1186086be978f
MohamedAmrrr9 <132684253+MohamedAmrrr9@users.noreply.github.com>
2023-05-05 19:54:14 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+ src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"
+
+    var typingrffect =new typed(".multitext",{
+        Strings :["MOHAMED ELREFAAY","Podcaster"],
+        loop :true,
+        typespeed :100,
+        backspeed:80,
+        backdelay :1500,
+    })
